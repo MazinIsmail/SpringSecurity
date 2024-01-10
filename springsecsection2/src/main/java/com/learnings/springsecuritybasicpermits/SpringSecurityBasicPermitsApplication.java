52024f7fbcf83d3db52e29c56ac20d6c08d6b46a
MazinIsmail <MazinIsmail>
2024-01-10 15:32:10 -0500

    Access URL specific

---
+++ springsecsection2/src/main/java/com/learnings/springsecuritybasicpermits/SpringSecurityBasicPermitsApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.springsecuritybasicpermits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicPermitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicPermitsApplication.class, args);
+	}
+
+}
